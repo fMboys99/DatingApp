a41ee1ae46cbb0820b7d9b7e046f0da65527da5f
fMboys99 <fmboys99@gmail.com>
2020-06-07 08:54:00 +0500

    End of Section 8

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -24,17 +24,6 @@ namespace DatingApp.API.Controllers
             _context = context;
         }
 
-        private static readonly string[] Summaries = new[]
-        {
-            "fMboys", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-            // private readonly ILogger<WeatherForecastController> _logger;
-
-            // public WeatherForecastController(ILogger<WeatherForecastController> logger)
-            // {
-            //     _logger = logger;
-            // }
         [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
@@ -43,18 +32,6 @@ namespace DatingApp.API.Controllers
 
             return Ok(values);
         }
-        // public IEnumerable<MyWeatherForecast> Get()
-        // {
-        //     var values = 
-        //     // var rng = new Random();
-        //     // return Enumerable.Range(1, 5).Select(index => new MyWeatherForecast
-        //     // {
-        //     //     Date = DateTime.Now.AddDays(index),
-        //     //     TemperatureC = rng.Next(-20, 55),
-        //     //     Summary = Summaries[rng.Next(Summaries.Length)]
-        //     // })
-        //     // .ToArray();
-        // }
 
         [AllowAnonymous]
         [HttpGet("{id}")]
